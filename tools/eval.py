50cf962b1c8253aab13c7ff1dee826a3b87878df
z-y-x123 <3521775583@qq.com>
2026-08-06 19:00:59 +0800

    优化了模型评估模块(tools/eval)，加入了保护代码以避免对主模块的重复导入

--- tools/eval.py
+++ tools/eval.py
@@ -18,32 +18,36 @@ from models.TransModel import TransModel
 from utils.loader import build_loader
 from utils.eval import eval
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--log_path', required=True, type=str)
-parser.add_argument('--append', type=str, default='best')
-parser.add_argument('--save_result', type=str, default='./results/')
-parser.add_argument('--batch_size', type=int, default=100)
-parser.add_argument('--beam_size', type=int ,default=1)
-eval_args = parser.parse_args()
+
+def main(eval_args):
+    # load checkpoint file
+    ckpt_path = os.path.join(eval_args.log_path, 'model' + eval_args.append + '.pth')
+    checkpoint = torch.load(ckpt_path, weights_only=False)
+
+    # update opt
+    opt = checkpoint['opt']
+    opt.beam_size = eval_args.beam_size
+    opt.batch_size = eval_args.batch_size
+
+    # build dataloader
+    dataset = SmilesSet(opt)
+    test_loader = build_loader(dataset, 'test', opt.batch_size, False)
+
+    model = TransModel(opt).cuda(opt.device_id)
+    model.load_state_dict(checkpoint['model'])
+
+    print('Testing')
+    results = eval(model, test_loader, opt)
+    for metric, score in results.items():
+        print(metric, score)
 
 
-# load checkpoint file
-ckpt_path = os.path.join(eval_args.log_path, 'model'+eval_args.append+'.pth')
-checkpoint = torch.load(ckpt_path,weights_only=False)
-
-# update opt
-opt = checkpoint['opt']
-opt.beam_size = eval_args.beam_size
-opt.batch_size = eval_args.batch_size
-
-# build dataloader
-dataset = SmilesSet(opt)
-test_loader = build_loader(dataset, 'test', opt.batch_size, False)
-
-model = TransModel(opt).cuda(opt.device_id)
-model.load_state_dict(checkpoint['model'])
-
-print('Testing')
-results = eval(model, test_loader, opt)
-for metric, score in results.items():
-    print(metric, score)
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--log_path', required=True, type=str)
+    parser.add_argument('--append', type=str, default='best')
+    parser.add_argument('--save_result', type=str, default='./results/')
+    parser.add_argument('--batch_size', type=int, default=100)
+    parser.add_argument('--beam_size', type=int, default=1)
+    args = parser.parse_args()
+    main(eval_args=args)
